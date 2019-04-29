506f1aed7c3fb3cfdbbb8f9a04b745fc406457a5
ChanMinPark <walkinpcm@gmail.com>
2019-04-29 23:59:11 +0900

    login page markup 틀 잡기.

---
+++ pages/account/login/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="page-account-login">
+    <!-- Animated Logo -->
+    <section>
+
+    </section>
+    
+    <!-- Input Group -->
+    <section>
+      <!-- ID -->
+      <!-- PW -->
+    </section>
+
+    <!-- Sign Up -->
+    <section>
+      
+    </section>
+
+    <!-- Social Login -->
+    <section>
+      
+    </section>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped></style>
